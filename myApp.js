052b3fb4940cb0d2c2122b26ceec23e3ff8d9227
brunodecruz <brunodecruz.05@gmail.com>
2024-01-27 22:02:14 -0300

    v1.2.3

--- myApp.js
+++ myApp.js
@@ -1,7 +1,17 @@
+require('dotenv').config();
+//import body parser
+let bodyParser = require('body-parser');
 let express = require('express');
+const req = require('express/lib/request');
 let app = express();
-console.log("Hello World");
+bodyParser.urlencoded({extended: false});
 
+console.log("Hello World");
+//ip logger
+app.use(function(req, res, next) {
+    console.log(req.method + " " + req.path + " - " + req.ip);
+    next();
+})
 app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/", function(req, res) {
@@ -9,10 +19,40 @@ app.get("/", function(req, res) {
 })
 
 app.get("/json", function(req, res) {
-    res.json({"message": "Hello json"});
+    let message = "Hello json";
+    if (process.env.MESSAGE_STYLE === "uppercase") {
+        message = message.toUpperCase();
+    }
+    res.json({"message": message});
 })
 
+app.get("/now", function(req, res, next) {
+    req.time = new Date().toString();
+    next();
+}
+, function(req, res) {
+    res.json({"time": req.time});
+}
+)
 
+app.get("/:word/echo", function(req, res) {
+    res.json({"echo": req.params.word});
+}
+)
+
+app.get("/name", function(req, res) {
+    let firstName = req.query.first;
+    let lastName = req.query.last;
+    res.json({"name": firstName + " " + lastName});
+}
+)
+
+app.post("/name", function(req, res) {
+    let firstName = req.body.first;
+    let lastName = req.body.last;
+    res.json({"name": firstName + " " + lastName});
+}
+)
 
 
 

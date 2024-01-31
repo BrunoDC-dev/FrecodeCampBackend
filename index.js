c2a3c8324fd501615f90c06da02bd5a093421c0d
brunodecruz <brunodecruz.05@gmail.com>
2024-01-30 22:52:04 -0300

    v1.2.6

--- index.js
+++ index.js
@@ -24,6 +24,21 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api", (req, res) => {
+  res.json({unix: Date.now(), utc: Date()});
+})
+app.get("/api/:date", (req, res) => {
+  let date = req.params.date;
+  if (/\d{5,}/.test(date)) {
+    date = +date;
+  }
+  let dateObj = new Date(date);
+  if (dateObj.toString() === "Invalid Date") {
+    res.json({error: "Invalid Date"});
+  } else {
+    res.json({unix: dateObj.valueOf(), utc: dateObj.toUTCString()});
+  }
+})
 
 
 // listen for requests :)

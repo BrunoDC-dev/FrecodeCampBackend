f1576033af138138a4e8f9885e3d9e95d78c3a75
brunodecruz <brunodecruz.05@gmail.com>
2024-01-31 22:20:35 -0300

    v1.3.0

--- server.js
+++ server.js
@@ -23,6 +23,9 @@ app.get("/", function (req, res) {
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
+app.get("/api/whoami", (req, res) => {
+  res.json({ipaddress: req.ip, language: req.headers["accept-language"], software: req.headers["user-agent"]});
+})
 
 app.get("/api", (req, res) => {
   res.json({unix: Date.now(), utc: Date()});
